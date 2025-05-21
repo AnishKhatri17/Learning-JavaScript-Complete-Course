88f9f8f867ae018e03d299d6ea8de7933ed95708
AnishKhatri17 <anish.khatri.135@gmail.com>
2025-05-21 11:30:57 +0545

    First concept in JavaScript using document.getElementById(); printing in console and using window.alert

---
+++ 1_Index_Page_JavaScript/index.js
@@ -0,0 +1,17 @@
+console.log(`Hello`);
+console.log("I love JavaScript");
+console.log('Thank You for this tutorial !');
+
+window.alert("Hello User, how are you today");
+window.alert("I am fine thanks for asking");
+
+document.getElementById("myH1").textContent = `This is H1 from using Javascript`;
+document.getElementById("myP").textContent = `This is a paragraph form using JavaScript`;
+
+// This is a single line comment ....
+
+/*
+    This is an 
+    example of a 
+    multiline comment ........
+ */
