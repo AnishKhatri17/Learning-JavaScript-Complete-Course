d171f77b996890b15b28dcf734c6164df7dfbb2c
AnishKhatri17 <anish.khatri.135@gmail.com>
2025-05-24 20:59:38 +0545

    Switch case statements to print the day of the week and Grades obtained based on the marks

---
+++ 13_Switch_Case_Statement/Switch.js
@@ -0,0 +1,74 @@
+// Switch case is another alternative for if-else statement .....
+
+//Example 1
+let day = "pizza";
+// let day = 6;
+
+switch(day){
+    case 1:
+        document.writeln('It is Sunday');
+        break;
+    case 2:
+        document.writeln('It is Monday');
+        break;
+    case 3:
+        document.writeln('It is Tuesday');
+        break;
+    case 4:
+        document.writeln('It is Wednesday');
+        break;
+    case 5:
+        document.writeln('It is Thursday');
+        break;
+    case 6:
+        document.writeln('It is Friday');
+        break;
+    case 7:
+        document.writeln('It is Saturday');
+        break;
+    default:
+        document.writeln(`${day} is an invalid output`);
+        break;
+}
+
+//Example 2
+
+let MarksObtained = 97;
+// let MarksObtained = 87;
+// let MarksObtained = 71;
+// let MarksObtained = 60;
+// let MarksObtained = 53;
+// let MarksObtained = 28;
+let Grade;
+
+switch(true){
+    case MarksObtained>=90:
+        Grade = "A+";
+        break;
+
+    case MarksObtained>=80:
+        Grade = "A";
+        break;
+
+    case MarksObtained>=70:
+        Grade = "B+";
+        break;
+
+    case MarksObtained>=60:
+        Grade = "B";
+        break;
+
+    case MarksObtained>=50:
+        Grade = "C+";
+        break;
+
+    case MarksObtained>=40:
+        Grade = "C";
+        break;
+
+    case MarksObtained<40:
+        Grade = "F";
+        break;
+}
+
+console.log(`Your Obtained Grade is : ` + Grade);
